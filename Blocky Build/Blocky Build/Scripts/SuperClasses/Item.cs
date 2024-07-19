a09b9cfc3de7dfc272ff861b5fc738039c6499a6
Sekaus <45533771+Sekaus@users.noreply.github.com>
2024-07-19 16:40:50 +0200

    Add files via upload

---
+++ Blocky Build/Blocky Build/Scripts/SuperClasses/Item.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public partial class Item : Node3D {
+    [Export]
+    public int MaxCount = 100;
+    [Export]
+    public string[] Tags;
+
+    public int Count = 1;
+    private string itemName = "";
+    public string ItemName {
+        get {
+            if (itemName != "")
+                return itemName;
+            else
+                return Name;
+        }
+        set {
+            itemName = value;
+        }
+    }
+}
---
+++ Blocky Build/Blocky Build/Scripts/WorldData.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System;
+public partial class WorldData : Node {
+    public enum WorldType {
+        Flat
+    }
+
+    public struct WorldLayer {
+        public string blockName;
+        public int height;
+    }
+
+    public WorldLayer[][] WorldTypeLayers = new WorldLayer[4][];
+
+    public WorldType Type = WorldType.Flat;
+
+    Register register;
+    public override void _Ready() {
+        register = GetParent().GetNode<Register>("%Register");
+        
+        WorldTypeLayers[(int)WorldType.Flat] = new[] {
+            new WorldLayer() { blockName = "Bedrock", height = 1 },
+            new WorldLayer() { blockName = "Stone", height = 4 },
+            new WorldLayer() { blockName = "Dirt", height = 1 },
+            new WorldLayer() { blockName = "GrassBlock", height = 1 },
+            new WorldLayer() { blockName = "Grass", height = 1 },
+        };
+    }
+}
